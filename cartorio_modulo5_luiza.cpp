9d267365672750ba0a416daed7dd862f97826f66
Luizadev1996 <146378990+Luizadev1996@users.noreply.github.com>
2023-09-28 16:09:42 -0300

    Alteração opção de saída
    Incluso no sistema a opção de saída

--- cartorio_modulo5_luiza.cpp
+++ cartorio_modulo5_luiza.cpp
@@ -123,6 +123,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sitema\n\n");
 		printf("Opção: ");
 		
 		scanf("%d", &opcao);
@@ -142,6 +143,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf ("Obrigada por utilizar o sistema\n");
+			return 0 ;
 			break;
 			
 			default:
